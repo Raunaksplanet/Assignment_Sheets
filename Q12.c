6e744ac07162fcb57c0e6548f0279e0ec61c8f18
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-12-08 22:15:34 +0530

    Create Q12.c

---
+++ Q12.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() 
+{
+    int a;
+    printf("Enter your age: "); scanf("%d",&a);
+
+    if(a>=80)
+    printf("You're retard");
+    else
+    printf("You're not retard");
+}
