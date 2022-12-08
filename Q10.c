4997e7fd34d35bf5b6c8e6ba03584ddd9caf6538
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-12-08 21:56:54 +0530

    Create Q10.c

---
+++ Q10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main() 
+{
+    int a,b,c;
+
+    printf("Enter First number: "); scanf("%d",&a);
+    printf("Enter Secound number: "); scanf("%d",&b);
+
+    printf("The power of given number is: %d",c = pow(a,b));
+}
