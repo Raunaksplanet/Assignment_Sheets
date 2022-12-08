dce799a032f0c40025633eda1ca5304d3cfa197c
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-12-08 20:55:54 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -6,6 +6,5 @@ float PI = 3.14, area;
 printf("Enter radius of circle: "); 
 scanf("%d", &r); 
 area = PI * r * r; 
-printf("\nArea of circle : %f ", area); 
-return 0; 
+printf("\nArea of circle : %f ", area);  
 }
