be05e700b4f66c0b2aad293c932b1b766594d4ed
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-12-08 21:27:09 +0530

    Create Q5.c

---
+++ Q5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    // Arithmetic operators in C programmig is +,-,*,/,%
+    int a = 10, b = 2;
+    printf("\n1.Addition operator: %d\n", a+b);
+    printf("2.Subtraction operator: %d\n", a-b);
+    printf("3.Multiply operator: %d\n", a*b);
+    printf("4.Division operator: %d\n", a/b);
+    printf("5.Modulus operator: %d\n", b%a);
+}
