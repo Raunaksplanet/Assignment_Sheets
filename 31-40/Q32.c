97c0289c75ada512ea3b14050d777dd73c98ceab
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-12-12 21:06:04 +0530

    Create Q32.c

---
+++ 31-40/Q32.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+	int i, j, k, c = 5, n = 5;
+	for (i = 1; i <= n; i++)
+	{
+		printf("%*c", (n - i + 1), ' ');
+		for (j = 1; j <= i; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	for (i = 1; i <= n - 1; i++)
+	{
+		printf("%*c", (i + 1), ' ');
+		for (j = 1; j <= n - i; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+}
