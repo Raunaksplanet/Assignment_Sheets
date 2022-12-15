24f35c73cbf1eae9485084bd7b87aaf7e44d5a0a
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-12-15 07:33:02 +0530

    Create Q33.c

---
+++ Q33.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	
+	int arr[10],a=0,i;
+	printf("Enter  10 element: "); 
+
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+		a = a + arr[i];
+	}
+
+	printf("The sum of 10 element is %d",a);
+}
