ac966c115d2d82b65ac532a8ed673d39a2d364fb
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-12-15 22:37:01 +0530

    Update Q38.c

--- 31-40/Q38.c
+++ 31-40/Q38.c
@@ -1,28 +1,24 @@
-#include <stdio.h>
-void main()
-{
-    int arr[11], N = 10, i, data, p;
-    printf("Enter array Elements Are: \n");
-
-    for (i = 0; i < N; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-
-    printf("Array Elementry are: \n");
-    for (i = 0; i < N; i++)
-        printf("%d\t", arr[i]);
-
-        printf("\nEnte data you want to add: \n"); scanf("%d",&data);
-        printf("\nEnte position number: \n"); scanf("%d",&p);
-
-        printf("\n Array Insertion Element are --\n\n");
-
-    for (i = N; i > p; i--)
-        arr[i] = arr[i - 1];
-    arr[p] = data;
-
-    N++;
-    for (i = 0; i < N; i++)
-        printf("%d\t", arr[i]);
-}
+#include <stdio.h>  
+int main ()  
+{  
+    int arr[10];  
+    int p = 0, i, n = 10;       
+    printf ("\nEnter %d elements in array: \n", n);        
+    for (i = 0; i < n; i++ )  
+    {   printf ("arr[%d] = ", i);  
+        scanf ("%d", &arr[i]);  
+    }  
+        for (i = p - 1; i < n -1; i++)  
+        {  
+            arr[i] = arr[i+1];
+        }  
+          
+        printf ("\nArray after deletion \n");  
+          
+         
+        for (i = 0; i< n - 1; i++)  
+        {  
+            printf ("arr[%d] = ", i);  
+            printf ("%d \n", arr[i]);  
+        }  
+    }   
