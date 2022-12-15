c9055b8b5017c04cd472860b364e3a080a6852f8
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-12-15 22:33:19 +0530

    Create Q39.c

---
+++ 31-40/Q39.c
@@ -0,0 +1,36 @@
+#include <stdio.h>  
+int main ()  
+{  
+    int arr[10];  
+    int p, i, n = 10;       
+    printf ("\nEnter %d elements in array: \n", n);        
+    for (i = 0; i < n; i++ )  
+    {   printf ("arr[%d] = ", i);  
+        scanf ("%d", &arr[i]);  
+    }  
+       
+    printf("Define the position of the array element where you want to delete: ");  
+    scanf ("%d", &p);  
+        
+    if (p >= n+1)  
+    {  
+        printf ("\nDeletion is not possible in the array.");  
+    }  
+    else  
+    {  
+        
+        for (i = p - 1; i < n -1; i++)  
+        {  
+            arr[i] = arr[i+1];
+        }  
+          
+        printf ("\nArray after deletion \n");  
+          
+         
+        for (i = 0; i< n - 1; i++)  
+        {  
+            printf ("arr[%d] = ", i);  
+            printf ("%d \n", arr[i]);  
+        }  
+    }  
+}  
