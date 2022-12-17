7d1a618f5a3e33f7e4531984ca8cc4f162da9ba9
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-12-17 16:44:24 +0530

    Create Q45.c

---
+++ 41-50/Q45.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int arr[3][3],i,j;
+    printf("Enter array element: \n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+        printf("[%d][%d] -- ",i,j);
+        scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("Array element are:");
+    for(i=0;i<3;i++)
+    {
+        printf("\n");
+        for(j=0;j<3;j++)
+        if(i==j)
+        printf("%d",arr[i][j]);
+        else
+        printf("  ");
+    }
+    
+}
