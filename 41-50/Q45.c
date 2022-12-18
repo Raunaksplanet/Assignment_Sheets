bcbc81f3f88e3808804094fb6560f40e3ed6432e
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-12-18 08:00:10 +0530

    Update Q45.c

--- 41-50/Q45.c
+++ 41-50/Q45.c
@@ -2,24 +2,24 @@
 
 int main(){
     int arr[3][3],i,j;
+
     printf("Enter array element: \n");
+
     for(i=0;i<3;i++)
     {
         for(j=0;j<3;j++)
         {
-        printf("[%d][%d] -- ",i,j);
-        scanf("%d",&arr[i][j]);
+            printf("[%d][%d] -- ",i,j);
+            scanf("%d",&arr[i][j]);
         }
     }
-    printf("Array element are:");
+    printf("Matrix is: ");
     for(i=0;i<3;i++)
     {
         printf("\n");
         for(j=0;j<3;j++)
-        if(i==j)
-        printf("%d",arr[i][j]);
-        else
-        printf("  ");
+        {
+            (i==j)?printf("%d\t",arr[i][j]):printf("  ");
+        }
     }
-    
 }
