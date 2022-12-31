79c490a89119f97998f81371be6fa12a678be0e3
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-01-01 00:28:36 +0530

    Update and rename Q1.c to Q52.c

--- 51-60/Q1.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    printf("Dezyne E'cole College");
-}
---
+++ 51-60/Q52.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main()
+{
+    char arr[20];
+    printf("Enter a single string: "); scanf("%s",arr);
+    printf("%s",arr);
+}
