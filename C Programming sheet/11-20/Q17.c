12fd84094d3026022c1b7c17dfaabe59901a2abb
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-03-24 23:28:32 +0530

    Update Q17.c

--- C Programming sheet/11-20/Q17.c
+++ C Programming sheet/11-20/Q17.c
@@ -1,34 +1,38 @@
+//q1 
 #include <stdio.h>
 void main()
 {
-    char name[20], clas[15];
-    float e, h, m, per, total;
-    printf("Enter Student Details\n");
-    printf("\nEnter Student Name -- ");
-    gets(name);
-    printf("\nEnter Student Class -- ");
-    gets(clas);
-    printf("\nEnter Hindi Marks -- ");
-    scanf("%f", &h);
-    printf("\nEnter English Marks --");
-    scanf("%f", &e);
-    printf("\nEnter Maths Marks -- ");
-    scanf("%f", &m);
-    total = e + h + m;
-    per = (total * 100) / 300;
-    printf("Student Name -- %s\n", name);
-    printf("Student Class -- %s\n", clas);
-    printf("Hindi Marks -- %.2f\n", h);
-    printf("English Marks -- %2.f\n", e);
-    printf("Maths Marks -- %2.f\n", m);
-    printf("Total Marks -- %2.f\n", total);
-    printf("Percentage -- %2.f\n", per);
-    if (per >= 60)
-        printf("\nFirst Division\n");
-    else if (per >= 48)
-        printf("\nSecond Division\n");
-    else if (per >= 36)
-        printf("\nThird Division\n");
-    else
-        printf("\nFail\n");
+	char name[20],class[15];
+	float eng,hind,math,per,total;
+
+	printf("Enter student details---\n\n");
+	printf("Enter student name: ");
+	gets(name);
+	printf("Enter student class: ");
+	gets(class);
+	printf("Enter student Hindi marks: ");
+	scanf("%f",&hind);
+	printf("Enter student English marks: ");
+	scanf("%f",&eng);
+	printf("Enter student Math marks: ");
+	scanf("%f",&math);
+
+	total = eng + hind + math;
+	per = (total * 100) / 300;
+
+	printf("\n\nStudent name: %s\n",name);
+	printf("Student class: %s\n",class);
+	printf("Hindi marks: %.2f\nEnglish marks: %.2f\nMath marks: %.2f",hind,eng,math);
+
+	printf("\nTotal marks: %.2f\n",total);
+	printf("Percentage: %.2f\n",per);
+
+	if(per>60)
+	printf("First divison\n");
+	else if(per>=48)
+	printf("Secound Divison\n");
+	else if(per>=36)
+	printf("Third Divison\n");
+	else
+	printf("Fail\n");
 }
