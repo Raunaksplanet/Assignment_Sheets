7179e912587e82d89c8632b570d39d9ae931630a
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-04-01 22:40:49 +0530

    Update q10.c

--- DSA sheet/1-10/q10.c
+++ DSA sheet/1-10/q10.c
@@ -1,37 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 int main()
 {
- 
-    int arr[10], n =10, i, j, pos;
-    printf("Enter elements in array---\n\n");
-    
-    for(i=0;i<n;i++)
+
+    int arr[10], n = 10, i, j, pos;
+    printf("Enter elements in array\n");
+
+    for (i = 0; i < n; i++)
     {
-        printf("Index No [%d] -- ",i);
-        scanf("%d",&arr[i]);
+        printf("Index No %d: ", i);
+        scanf("%d", &arr[i]);
+    }
+    system("cls");
+    printf("Array Elements are\n");
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d  ", arr[i]);
     }
 
-    printf("Array Elements are---\n\n");
+    printf("\n\nEnter a position to delete element: ");
+    scanf("%d", &pos);
 
-    for(i=0;i<n;i++)
+    for (i = pos - 1; i < n; i++)
     {
-        printf("%d  ",arr[i]);
-    }
-
-    printf("\n\nEnter a position to delete element: "); scanf("%d", &pos);
-
-    for(i=pos-1;i<n;i++)
-    {
-        arr[i] = arr[i+1];
+        arr[i] = arr[i + 1];
     }
     n--;
-     
-    printf("Array Elements After deletion---\n\n");
 
-    for(i=0;i<n;i++)
+    printf("\nArray Elements After deletion\n\n");
+
+    for (i = 0; i < n; i++)
     {
-        printf("%d  ",arr[i]);
+        printf("%d  ", arr[i]);
     }
-
 }
-// q10
