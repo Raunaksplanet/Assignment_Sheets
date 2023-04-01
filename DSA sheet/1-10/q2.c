be0ff606109f7a577d72b7bdaadb1515406114e8
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-04-02 00:21:07 +0530

    Update q2.c

--- DSA sheet/1-10/q2.c
+++ DSA sheet/1-10/q2.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
 
+    int n, *ptr, i;
+
+    printf("Enter the size of Memory: ");
+    scanf("%d", &n);
+
+    ptr = (int *)calloc(n , sizeof(int));
+
+    printf("Enter Elements in array\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("Index no %d: ", i);
+        scanf("%d", &ptr[i]);
+    }
+    printf("\nElements in array\n\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("Index no %d: %d\n", i, ptr[i]);
+    }
+}
