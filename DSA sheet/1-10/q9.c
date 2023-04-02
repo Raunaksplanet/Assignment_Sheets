c1bd9b2ba24f52932175af2bc78e63a3735136bd
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-04-02 12:44:51 +0530

    Update q9.c

---
+++ DSA sheet/1-10/q9.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+
+    int arr[10],i,n=10;
+
+    printf("Enter elements in array\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Elements in array\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d  ", arr[i]);
+    }
+
+    for(i=9;i<n;i++)
+    {
+        arr[i] = arr[i+1];
+    }
+    n--;
+
+    printf("\n\nElements in array\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d  ", arr[i]);
+    }
+    printf("\n");
+
+}
