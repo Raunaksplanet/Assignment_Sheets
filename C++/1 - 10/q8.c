8b3966297f6950f0c5820455bb385a40faa90793
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-05-11 09:48:45 +0530

    Create q8.c

---
+++ C++/1 - 10/q8.c
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << "C++ Manipulator using setw functions"<< endl;
+    cout << setw(4) << "1024" << endl;
+    cout << setw(4) << "512" << endl;
+    cout << setw(4) << "64" << endl;
+    cout << setw(4) << "8" << endl;
+    cout << setw(4) << "-----" << endl;
+    cout << setw(4) << "1608" << endl;
+}
