82c371375cccc53f258f29665da28dc4c9135e0a
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-05-23 10:33:24 +0530

    Update Q66.c

--- C Programming sheet/61-70/Q66.c
+++ C Programming sheet/61-70/Q66.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int returnValue(int a)
+{
+    return a;
+}
+
+int main()
+{
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+
+    
+    printf("Returned value is %d", returnValue(a));
+
+}
 
